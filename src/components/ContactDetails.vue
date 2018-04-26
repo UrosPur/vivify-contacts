21d2256c6d9fb0ba780b98f3b22e70dd5351e4d0
UrosPurtic <uros.purtic@gmail.com>
2018-04-26 21:56:59 +0200

    almoust done-list and details imported

---
+++ src/components/ContactDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Contact details
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContactDetails"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
-        contacts
+        <div class="container">
+            <div class="row">
+
+                <div class="col">
+                    <ContactList/>
+                </div>
+
+                <div class="col">
+                    <ContactDetails/>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
+    import ContactList from '../components/ContactList'
+    import ContactDetails from '../components/ContactDetails'
+
     export default {
-        name: "Contacts"
+
+        name: "Contacts",
+
+        components: {
+
+            ContactList,
+            ContactDetails
+        }
+
     }
+
+
 </script>
 
 <style scoped>
